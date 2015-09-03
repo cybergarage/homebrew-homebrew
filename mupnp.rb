0c02fa102ed4786f57e7e1c68be4050d7d355319
Satoshi Konno <skonno@cybergarage.org>
2015-09-03 12:21:19 +0900

    * Updated to use v3.0.1.

--- mupnp.rb
+++ mupnp.rb
@@ -1,7 +1,7 @@
 class Mupnp < Formula
   homepage "https://github.com/cybergarage/mupnpc"
-  url "https://github.com/cybergarage/mupnpc/archive/3.0.0.tar.gz"
-  sha1 "e5bf8874dbb2f0f5d75a89050a8f52e3d6a71961"
+  url "https://github.com/cybergarage/mupnpc/archive/3.0.1.tar.gz"
+  sha1 "e39b88efefda06884610e32af4914c6687c29030"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -9,7 +9,8 @@ class Mupnp < Formula
 
   def install
     system "./bootstrap"
-    system "./configure_macosx", "--disable-debug",
+    system "./configure_macosx", "--enable-libxml2",
+                                 "--disable-debug",
                                  "--disable-dependency-tracking",
                                  "--disable-silent-rules",
                                  "--prefix=#{prefix}"
