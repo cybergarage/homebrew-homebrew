ff499ab6f3ffaf22d7bf853d269d5cdfbb746cda
Satoshi Konno <skonno@cybergarage.org>
2015-09-15 22:16:30 +0900

    * Added uecho.rb.

---
+++ uecho.rb
@@ -0,0 +1,16 @@
+class Mupnp < Formula
+  homepage "https://github.com/cybergarage/uecho"
+  url "https://github.com/cybergarage/uecho/archive/0.9.tar.gz"
+  sha1 "b34cf4e9e1f4f9ef94af4bf3ae249a8ddd27c923"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  def install
+    system "./bootstrap"
+    system "./configure_macosx", "--disable-examples",
+                                 "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+end
