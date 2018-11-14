b705f413fa675824c3a6c34ee7bc74e8c49ad196
Satoshi Konno <skonno@cybergarage.org>
2018-11-14 10:15:44 +0900

    Update for uecho 0.9.2

--- uecho.rb
+++ uecho.rb
@@ -1,7 +1,7 @@
 class Uecho < Formula
   homepage "https://github.com/cybergarage/uecho"
-  url "https://github.com/cybergarage/uecho/archive/0.9.tar.gz"
-  sha256 "49c50c6f199a848a255c4546e4679a58c8a5d7fe8abadc219073ab647f116036"
+  url "https://github.com/cybergarage/uecho-c/archive/0.9.2.tar.gz"
+  sha256 "e2eee4028cfe390a5f76f1c4335b2bc3b8652740467e8cff264f32130491e39a"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
