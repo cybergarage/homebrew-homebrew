c55eb4fc62f87de0cd770530571a7de098eaa9f0
Satoshi Konno <skonno@cybergarage.org>
2022-05-06 21:24:33 +0900

    Update uecho to v1.2.0

--- uecho.rb
+++ uecho.rb
@@ -1,7 +1,7 @@
 class Uecho < Formula
   homepage "https://github.com/cybergarage/uecho"
-  url "https://github.com/cybergarage/uecho/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "01e49838a8490e5b4f101ed2973ccf9bc163ed3714d2833664f5bf80c3e66c0e"
+  url "https://github.com/cybergarage/uecho/archive/refs/tags/1.2.0.tar.gz"
+  sha256 "53259aadc79ce2c9e9316571685d10580dd20cbafb6c6d58f259d8f121a6785f"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
